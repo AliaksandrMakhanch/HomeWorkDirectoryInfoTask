35ebe1a239c94b86e396755023f9940169f43e62
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2023-10-31 17:37:58 +0300

    Add project files.

---
+++ HomeWorkDirectoryInfoTask/FileSystemVisitor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileSystemView 
+{
+    public class FileSystemVisitor
+    {
+        private readonly DirectoryInfo _rootDirectory;
+        private readonly FilterDelegate _filter;
+
+        public delegate bool FilterDelegate(FileSystemInfo item);
+
+        public FileSystemVisitor(string rootDirectoryPath) : this(rootDirectoryPath, null)
+        {
+        }
+
+        public FileSystemVisitor(string rootDirectoryPath, FilterDelegate filter)
+        {
+            _rootDirectory = new DirectoryInfo(rootDirectoryPath);
+            _filter = filter ?? DefaultFilter;
+        }
+
+        private static bool DefaultFilter(FileSystemInfo item)
+        {
+            return true;
+        }
+
+        public IEnumerable<FileSystemInfo> Traverse()
+        {
+            Stack<DirectoryInfo> directories = new Stack<DirectoryInfo>();
+            directories.Push(_rootDirectory);
+
+            while (directories.Count > 0)
+            {
+                DirectoryInfo currentDirectory = directories.Pop();
+
+                FileSystemInfo[] items;
+                try
+                {
+                    items = currentDirectory.EnumerateFileSystemInfos().ToArray();
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+
+                foreach (FileSystemInfo item in items)
+                {
+                    if (item is DirectoryInfo itemDirectory)
+                    {
+                        directories.Push(itemDirectory);
+                    }
+
+                    if (_filter(item))
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HomeWorkDirectoryInfoTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FileSystemView 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectoryPath = @"C:\Users\Aliaksandr_Makhnach\source\repos\FileSystemHomeWork\";
+
+            Console.WriteLine("Common list of all files and subfolders:");
+            var fileSystemVisitor = new FileSystemVisitor(rootDirectoryPath);
+            foreach (var item in fileSystemVisitor.Traverse())
+            {
+                Console.WriteLine(item.FullName);
+            }
+
+            Console.WriteLine("\nJust subfolders:");
+            FileSystemVisitor.FilterDelegate filter = (item) => item is DirectoryInfo;
+            var filteredFileSystemVisitor = new FileSystemVisitor(rootDirectoryPath, filter);
+            foreach (var item in filteredFileSystemVisitor.Traverse())
+            {
+                Console.WriteLine(item.FullName);
+            }
+        }
+    }
+}
